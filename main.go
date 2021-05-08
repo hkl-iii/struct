eeedfa14d749f1fe7006c6244ff749072d471976
hkl-iii <rahleggon@gmail.com>
2021-05-07 22:05:07 -0500

    created new print func line 29

--- main.go
+++ main.go
@@ -10,18 +10,22 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName  string
-	contact   contactInfo
+	contactInfo
 }
 
 func main() {
 	jim := person{
 		firstName: "Jim",
 		lastName:  "Party",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:   "jim@gmail.com",
 			zipCode: 94000,
 		},
 	}
 
-	fmt.Printf("%+v", jim)
+	jim.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
