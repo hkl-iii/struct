89ab86c38472d3c9fc91d6be252fcbcc56411782
hkl-iii <rahleggon@gmail.com>
2021-05-07 23:13:43 -0500

    added pointers

--- main.go
+++ main.go
@@ -23,11 +23,13 @@ func main() {
 		},
 	}
 
+	jimPointer := &jim
+	jimPointer.updateName("jimmy")
 	jim.print()
 }
 
-func (p person) updateName(newFirstName string) {
-	p.firstName = newFirstName
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
 }
 
 func (p person) print() {
